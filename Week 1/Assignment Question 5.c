345a1ee546eb75608423a6e755a5447ad500e885
Subham Sworup Mallick <subhamsworupmallick@gmail.com>
2023-03-11 17:20:29 +0530

    Update and rename Assignment Question 5 to Assignment Question 5.c

--- Week 1/Assignment Question 5.c
+++ Week 1/Assignment Question 5.c
@@ -6,17 +6,16 @@ int main()
 {
     float r, d, c, a;
 
-    printf("Enter the radius of the circle::\n");
+    printf("Enter the radius of the circle:\n");
     scanf("%f", &r);
 
     d = 2 * r;
     c = 2 * 3.14 * r;
     a = 3.14 * (r * r);
 
-    printf("\n");
-    printf("Diameter      :: = %f units \n", d);
-    printf("Circumference :: = %f units \n", c);
-    printf("Area          :: = %f sq. units ", a);
+    printf("Diameter = %f units \n", d);
+    printf("Circumference = %f units \n", c);
+    printf("Area = %f sq. units ", a);
     
     return 0;
 }
