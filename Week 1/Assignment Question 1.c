b5be47bc2fc4fd5e839c3c1e5bfa58fd0cdf2e7f
Subham Sworup Mallick <subhamsworupmallick@gmail.com>
2023-03-13 22:09:07 +0530

    Update Assignment Question 1.c

--- Week 1/Assignment Question 1.c
+++ Week 1/Assignment Question 1.c
@@ -7,20 +7,23 @@ int main()
     int i;
     for (i=1; i<=100; i++)
     {
-        if ((i%3) == 0) 
+    if (i%3==0 && i%5==0)
+        {
+        printf("FizzBuzz\t");
+        }
+        else if (i%3== 0) 
         {
             printf("Fizz\t"); 
         }
-        else if ((i%5) == 0) 
+        else if (i%5== 0) 
         {                     
-            printf("Buzz\t");   
-        }
+            printf("Buzz\t"); 
+         }  
         else 
         {
             printf("%d\t", i);   
         }
  
     }
- 
     return 0;
 }
