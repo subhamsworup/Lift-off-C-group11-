ea987fd1754ae2be55298ca599a6cd8f8f946f7e
Subham Sworup Mallick <subhamsworupmallick@gmail.com>
2023-03-19 18:34:45 +0530

    Create Assignment Question 1.c

---
+++ Week 2/Assignment Question 1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int countoccur(int arr[], int n, int num) {
+    int count = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == num) {
+            count++;
+        }
+    }
+    return count;
+}
+int main() {
+    int arr[] = {1, 2, 5, 4, 5, 6, 7, 8, 3, 10};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int num = 5; 
+    int count = countOccurrences(arr, n, num);
+    printf("The number of occurrences of %d in the array is %d\n", num, count);
+    return 0;
+}
