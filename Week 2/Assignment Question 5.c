f596b515154eaaaf987fe3a41d04beda7c8948cc
Subham Sworup Mallick <subhamsworupmallick@gmail.com>
2023-03-20 23:18:28 +0530

    Create Assignment Question 5.c

---
+++ Week 2/Assignment Question 5.c
@@ -0,0 +1,26 @@
+/* Write a C program to copy the elements of one array into another array */
+
+#include <stdio.h>
+int main()
+{
+	int a[5] = { 3, 6, 9, 2, 5 }, n = 5;
+	int b[n], i;
+	
+	for (i = 0; i < n; i++)
+	{
+		b[i] = a[i];
+	}
+	
+	printf("The first array is :");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+  
+	printf("\nThe second array is :");
+	for (i = 0; i < n; i++) 
+	{
+		printf("%d ", b[i]);
+	}
+	return 0;
+}
