f8164509c99a0272ec51795a5f17ca8f4efe5c89
Subham Sworup Mallick <subhamsworupmallick@gmail.com>
2023-03-21 20:55:07 +0530

    Create Assignment Question 10.c

---
+++ Week 2/Assignment Question 10.c
@@ -0,0 +1,34 @@
+/* Write a program to find the total number of alphabets , digits and special characters in a string */
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    int i,alphabets = 0, digits = 0, specialchar = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for (i= 0; i < strlen(str); i++)
+    {
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) 
+         {
+            alphabets++;
+         }
+        else if (str[i] >= '0' && str[i] <= '9')
+         {
+            digits++;
+         }
+        else 
+        {
+            specialchar++;
+        }
+    }
+
+    printf("Number of alphabets: %d\n", alphabets);
+    printf("Number of digits: %d\n", digits);
+    printf("Number of special characters: %d\n", specialchar);
+
+    return 0;
+}
