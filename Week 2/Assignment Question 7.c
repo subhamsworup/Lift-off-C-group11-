e0ad9ae613a76afe14a1ba8951ec4f9275166c1e
Subham Sworup Mallick <subhamsworupmallick@gmail.com>
2023-03-21 12:23:16 +0530

    Create Assignment Question 7.c

---
+++ Week 2/Assignment Question 7.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    int s, i, j, temp;
+    printf("Enter the size of the array: ");
+    scanf("%d", &s);
+    int arr[s];
+    
+    printf("Enter positive and negative integers: ");
+    for (i = 0; i < s; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    j = 0;
+    
+    for (i = 0; i < s; i++)
+    {
+     if (arr[i] < 0)
+    {
+     if (i != j)
+    {
+      temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+    j++;
+    }
+    }
+     printf("The modified array is ");
+
+   for (i = 0; i < s; i++)
+  {
+   printf("%d ", arr[i]);
+  }
+   return 0;
+}
+
